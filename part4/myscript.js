3e7b4dc454e094a11360908f054c907901b9ae5c
Pallavi <pallaviganji21@gmail.com>
2024-08-25 21:00:22 +0530

    Added js

---
+++ part4/myscript.js
@@ -0,0 +1,12 @@
+function calc(){
+    const result = textNum.value * textNum.value;
+    lblAnswer.innerHTML = result
+}
+function calc()
+{
+    let a =10;
+    let b=20;
+    let c=a+b;
+    lblAnswer.innerHTML = c;
+    alert(c)
+}  
